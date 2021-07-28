e32d65c377f3de726d659e07dce06631ece0df69
Yunjeong Choi <yunjeongchoi@Yunjeongui-MacBookAir.local>
2021-07-28 15:32:37 +0900

    초기 세팅

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+// Format on document(정렬하기)
+//   - shift + cmd + p -> format document 엔터
+const App = () => {
+  return <div className="App">
+    <ul className="flex-container">
+      <li className="flex-item">1</li>
+      <li className="flex-item">1</li>
+      <li className="flex-item">1</li>
+      <li className="flex-item">1</li>
+      <li className="flex-item">1</li>
+      <li className="flex-item">1</li>
+      <li className="flex-item">1</li>
+      <li className="flex-item">1</li>
+      <li className="flex-item">1</li>
+    </ul>
+  </div>;
+}
+
+export default App;
